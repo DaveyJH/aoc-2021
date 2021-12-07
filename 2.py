bcd93cd551cf44431bf9bda4011937fcf43868e6
DaveyJH <daveyj.dh@googlemail.com>
2021-12-07 22:02:41 +0000

    feat: Add day 2 part 2

--- 2.py
+++ 2.py
@@ -10,6 +10,8 @@ for val in origin_data:
     new_val = val.rstrip("\n")
     formatted_data.append(new_val)
 
+# Part 1
+#######################################
 forward = 0
 y_position = 0
 
@@ -21,5 +23,21 @@ for data in formatted_data:
     elif "up" in data:
         y_position = y_position - int(data[data.index(" "):])
 
-
 print(forward * y_position)
+
+# part 2
+#######################################
+x_pos = 0
+y_pos = 0
+aim = 0
+
+for data in formatted_data:
+    if "forward" in data:
+        x_pos = x_pos + int(data[data.index(" "):])
+        y_pos = y_pos + aim * int(data[data.index(" "):])
+    elif "down" in data:
+        aim = aim + int(data[data.index(" "):])
+    elif "up" in data:
+        aim = aim - int(data[data.index(" "):])
+
+print(x_pos * y_pos)
