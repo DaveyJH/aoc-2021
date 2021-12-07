5f3356b29bf04cf9aed5d24ee11c88ed8cf36c31
DaveyJH <daveyj.dh@googlemail.com>
2021-12-07 21:47:13 +0000

    feat: Add day 1 part 2

--- 1.py
+++ 1.py
@@ -10,10 +10,19 @@ for val in data:
     new_val = val.rstrip("\n")
     formatted_data.append(int(new_val))
 
-result = 0
+first_result = 0
 
 for i in range(1, len(formatted_data)):
     if formatted_data[i] > formatted_data[i - 1]:
+        first_result += 1
+
+print(first_result)
+
+fd = formatted_data
+result = 0
+
+for i in range(3, len(fd)):
+    if fd[i] + fd[i - 1] + fd[i - 2] > fd[i - 1] + fd[i - 2] + fd[i - 3]:
         result += 1
 
 print(result)
