737af28d1f490a9d5ce1aae81e13b7779128b77c
DaveyJH <daveyj.dh@googlemail.com>
2021-12-08 14:29:09 +0000

    docs: Add basic template + text file for test data

---
+++ template.py
@@ -0,0 +1,13 @@
+# replace ??? with current day
+# add test data to input-data/advent-test.txt
+# replace test with current day when happy with script
+
+# https://adventofcode.com/2021/day/???
+# https://adventofcode.com/2021/day/???/input
+
+with open("input-data/advent-test.txt", "r") as f:
+    data = f.readlines()
+
+formatted_data = []
+
+# format data as necessary
