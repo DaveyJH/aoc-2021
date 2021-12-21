277a7c799a5ca5e9ab2d2d0f9cb26928e7c06407
DaveyJH <daveyj.dh@googlemail.com>
2021-12-21 12:47:27 +0000

    feat: Add day 11 part 1

--- 11.py
+++ 11.py
@@ -7,7 +7,7 @@
 
 from pprint import pprint
 
-with open("input-data/advent-test.txt", "r") as f:
+with open("input-data/advent-11.txt", "r") as f:
     data = f.readlines()
 
 formatted_data = [line.rstrip("\n") for line in data]
@@ -16,6 +16,7 @@ flashes = 0
 
 
 def create_grid(origin):
+    """Create array of row arrays to represent grid"""
 
     new_grid = []
     for row in origin:
@@ -27,6 +28,7 @@ def create_grid(origin):
 
 
 def add_one(current_grid):
+    """Add one to all octos"""
 
     new_grid = []
     for row in current_grid:
@@ -35,12 +37,11 @@ def add_one(current_grid):
             octo += 1
             new_line.append(octo)
         new_grid.append(new_line)
-    return(new_grid)
+    return new_grid
 
 
-
-# Last adjustment is not happening
 def check_surrounds(current_grid):
+    """Check for octos to be manipulated and keep flash count"""
 
     flash_count = 0
     affected_octos = []
@@ -48,8 +49,8 @@ def check_surrounds(current_grid):
 
     for i, row in enumerate(current_grid):
         for j, octo in enumerate(row):
-            if octo == 9:
-                flashing_octos.append((i,j))
+            if octo > 9:
+                flashing_octos.append((i, j))
                 flash_count += 1
                 surround = []
                 if i > 0:
@@ -76,42 +77,47 @@ def check_surrounds(current_grid):
 
 
 def check_flashes(current_grid):
+    """Check for grid changes according to flashes"""
 
     new_grid = list(i[:] for i in current_grid)
 
     global flashes
     surrounds_and_flashes = check_surrounds(current_grid)
+    # add to flash count
     flashes += surrounds_and_flashes[1]
+    # create lis of coords
     adjustment_octos = []
     for i in surrounds_and_flashes[0]:
         adjustment_octos.extend(i)
 
-    print(adjustment_octos)
-    adjustment_octos.extend(surrounds_and_flashes[2])
-    print(adjustment_octos)
+    reset_octos = surrounds_and_flashes[2]
 
+    # increase affected octos
     for coord in adjustment_octos:
         point = new_grid[coord[0]][coord[1]]
         if point != 0:
             new_grid[coord[0]][coord[1]] += 1
+
+    # reset flashed octos
+    for coord in reset_octos:
+        point = new_grid[coord[0]][coord[1]]
         if point >= 9:
             new_grid[coord[0]][coord[1]] = 0
 
-    print(adjustment_octos)
-    pprint(new_grid)
-    print("----------------------")
     if new_grid != current_grid:
         new_grid = check_flashes(new_grid)
-    return new_grid
-    # print(adjustment_octos)
 
-    # return new_grid
+    return new_grid
 
 
 grid = create_grid(formatted_data)
-# print(grid)
-grid = add_one(grid)
-print(grid)
-check_flashes(grid)
-# print(check_flashes(grid))
+
+for _ in range(100):
+    grid = add_one(grid)
+    grid = check_flashes(grid)
+
+print("Last Grid ", "-" * 50)
+pprint(grid)
+print("-" * 60)
+
 print(f"flashes = {str(flashes)}")
